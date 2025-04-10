3f80a31a778cc21f64bfdaa2bee04d57202412ed
abdurrahmanc-dev <abdurrahmanc@lambdacrunch.com>
2025-04-10 18:18:13 +0600

    settings updated with allowed hosts

--- tracker/settings.py
+++ tracker/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-wlz$e*xg+g@&o_*_l*(vqxi&12hp!zib71fd3&urk&ro#dr!ho
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
